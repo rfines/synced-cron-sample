2b0c0c2db571189915a86130ad1b3abe699af01b
Robert Fines <fines.robert@gmail.com>
2015-07-28 11:06:32 -0500

    refactored a bit to match my conditions.

--- server/app.js
+++ server/app.js
@@ -1,32 +1,10 @@
-SyncedCron.add({
- 	name: 'sample method',
-    schedule: function(parser) {
-        // parser is a later.parse object
-         //"every 2 minutes"at 2:01 am and 2:01 pm and 8:01 pm
-        return parser.text("every 2 minutes");
-    }, 
-    job: function() {
-        console.log("Running sample method");
-        Meteor.subscribe("logs", function(){
-        	Meteor.call("insertLog", function(e,r){
-	        	return;
-	        });	
-        });
-        
-    }
-});
 
-Meteor.methods({
-	insertLog:function(){
-		return Logs.insert({startedAt:new Date(), message:"Starting sample method"});
-	}
-})
 Meteor.publish("logs", function(){
-	
+	console.log("publishing logs");
 	return Logs.find();
 });
 
 Meteor.startup(function () {
 // code to run on server at startup
-	SyncedCron.start();
+	
 });
